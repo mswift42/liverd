fa33b5a08accbcdc29da689815b1491ae9d0e69b
mswift42 <martin.haesler@gmail.com>
2016-04-27 17:25:58 +0100

    Import and display toolbar-component

---
+++ lib/components/res-component/res_component.dart
@@ -0,0 +1,13 @@
+import 'package:angular2/core.dart' show Component;
+import 'package:liverd/components/toolbar-component/toolbar_component.dart';
+
+@Component(
+    selector: 'res-component',
+    templateUrl: 'res_component.html',
+    styleUrls: const ['res_component.css'],
+    directives: const [ToolbarComponent]
+)
+
+class ResComponent {
+
+}
