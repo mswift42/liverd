25380e22c1c472ccc5d079fe6a8ac30b80157a70
mswift42 <martin.haesler@gmail.com>
2016-04-27 17:25:22 +0100

    Add class ToolbarComponent

--- lib/components/toolbar-component/toolbar_component.dart
+++ lib/components/toolbar-component/toolbar_component.dart
@@ -1,1 +1,7 @@
 import 'package:angular2/core.dart' show Component;
+
+@Component(
+    selector: 'toolbar-component',
+    templateUrl: 'toolbar_component.html',
+    styleUrls: const ['toolbar_component.css'])
+class ToolbarComponent {}
