a2e385aa4468f5f8a8893894d6cd02ef66f63fc5
mswift42 <martin.haesler@gmail.com>
2016-04-28 15:40:35 +0100

    Add contact inputs

---
+++ lib/components/contact-component/contact_component.dart
@@ -0,0 +1,16 @@
+import 'package:angular2/core.dart' show Component, Input;
+import 'package:liverd/components/section-component/section_component.dart';
+
+@Component(
+    selector: 'contact-component',
+    templateUrl: 'contact_component.html',
+    styleUrls: const ['constact_component.css'],
+    directives: const [SectionComponent])
+class ContactComponent {
+  @Input()
+  String phone;
+  @Input()
+  String email;
+  @Input()
+  String url;
+}
