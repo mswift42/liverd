913223ec80d18e80e90c54a8070fec77997a70f1
mswift42 <martin.haesler@gmail.com>
2016-04-28 15:58:45 +0100

    Import and display contact component

--- lib/components/res-component/res_component.dart
+++ lib/components/res-component/res_component.dart
@@ -1,13 +1,26 @@
-import 'package:angular2/core.dart' show Component;
+import 'package:angular2/core.dart';
 import 'package:liverd/components/toolbar-component/toolbar_component.dart';
+import 'package:liverd/components/contact-component/contact_component.dart';
+import 'package:liverd/services/ResumeService.dart';
+import 'package:liverd/services/Resume.dart';
 
 @Component(
     selector: 'res-component',
     templateUrl: 'res_component.html',
     styleUrls: const ['res_component.css'],
-    directives: const [ToolbarComponent]
+    directives: const [ToolbarComponent, ContactComponent],
+    providers: const [ResumeService]
 )
 
-class ResComponent {
+class ResComponent implements OnInit {
+  Resume res;
+  Contact contact;
+  final ResumeService _resumeService;
 
+  ResComponent(this._resumeService);
+
+  ngOnInit() {
+    res = _resumeService.sampleResume();
+    contact = res.contact;
+  }
 }
