cad55a569df4ff96370a9a43757b8059953a1442
mswift42 <martin.haesler@gmail.com>
2016-04-28 15:31:21 +0100

    Add html, css files

---
+++ lib/components/section-component/section_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular2/core.dart' show Component;
+
+@Component(
+    selector: 'section-component',
+    templateUrl: 'section_component.html',
+    styleUrls: const ['section_component.css']
+)
+
+class SectionComponent {}
