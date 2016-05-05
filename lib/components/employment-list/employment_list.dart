de2feabb4caacb61cf06d7883b2ac6d4e7ceeb14
mswift42 <martin.haesler@gmail.com>
2016-05-05 19:32:22 +0100

    Add employmentlist component

---
+++ lib/components/employment-list/employment_list.dart
@@ -0,0 +1,17 @@
+import 'package:angular2/core.dart' show Component;
+import 'package:liverd/components/employment-component/employment_component.dart';
+import 'package:liverd/services/Resume.dart';
+import 'package:liverd/services/ResumeService.dart';
+
+@Component(
+    selector: 'employment-list',
+    templateUrl: 'employment_component.html',
+    styleUrls: const ['employment_component.css'],
+    directives: const [EmploymentComponent])
+class EmploymentList {
+  List<Employment> employmentlist;
+
+  EmploymentList(ResumeService _resumeService) {
+    employmentlist = _resumeService.sampleResume().employment;
+  }
+}
