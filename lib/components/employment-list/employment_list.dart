52fbdc1ea5d431ac5a0b2f44491c148cde041594
mswift42 <martin.haesler@gmail.com>
2016-05-05 19:37:27 +0100

    Wrap in section component

--- lib/components/employment-list/employment_list.dart
+++ lib/components/employment-list/employment_list.dart
@@ -1,4 +1,5 @@
 import 'package:angular2/core.dart' show Component;
+import 'package:liverd/components/section-component/section_component.dart';
 import 'package:liverd/components/employment-component/employment_component.dart';
 import 'package:liverd/services/Resume.dart';
 import 'package:liverd/services/ResumeService.dart';
@@ -7,7 +8,7 @@ import 'package:liverd/services/ResumeService.dart';
     selector: 'employment-list',
     templateUrl: 'employment_component.html',
     styleUrls: const ['employment_component.css'],
-    directives: const [EmploymentComponent])
+    directives: const [EmploymentComponent, SectionComponent])
 class EmploymentList {
   List<Employment> employmentlist;
 
