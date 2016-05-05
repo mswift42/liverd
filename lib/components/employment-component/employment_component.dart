6bb054e915622b08de1703f2df4d601c2c13ac90
mswift42 <martin.haesler@gmail.com>
2016-05-05 19:41:27 +0100

    Add employment component markup

--- lib/components/employment-component/employment_component.dart
+++ lib/components/employment-component/employment_component.dart
@@ -1,16 +1,16 @@
 import 'package:angular2/core.dart';
-import 'package:liverd/services/Resume.dart';
-import 'package:liverd/services/ResumeService.dart';
 
 @Component(
     selector: 'employment-component',
-templateUrl: 'employment_component.html',
-styleUrls: const ['employment_component.css']
-)
-
+    templateUrl: 'employment_component.html',
+    styleUrls: const ['employment_component.css'])
 class EmploymentComponent {
-  @Input() String period;
-  @Input() String company;
-  @Input() String position;
-  @Input() String description;
+  @Input()
+  String period;
+  @Input()
+  String company;
+  @Input()
+  String position;
+  @Input()
+  String description;
 }
