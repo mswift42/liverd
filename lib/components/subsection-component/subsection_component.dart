952c8495624478fcd46638ba973f85f8d04a5632
mswift42 <martin.haesler@gmail.com>
2016-06-05 08:34:08 +0100

    Add subsection component

---
+++ lib/components/subsection-component/subsection_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'subsection-component',
+templateUrl: 'subsection_component.html',
+styleUrls: const ['subsection_component.css']
+)
+
+class SubsectionComponent {}
