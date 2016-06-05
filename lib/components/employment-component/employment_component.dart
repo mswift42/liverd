dc82f7de198ad1223034070c6ed4e3dd4b3c0779
mswift42 <martin.haesler@gmail.com>
2016-06-05 08:36:07 +0100

    Use Subsection component for employment sections

--- lib/components/employment-component/employment_component.dart
+++ lib/components/employment-component/employment_component.dart
@@ -1,9 +1,11 @@
 import 'package:angular2/core.dart';
+import 'package:liverd/components/subsection-component/subsection_component.dart';
 
 @Component(
     selector: 'employment-component',
     templateUrl: 'employment_component.html',
-    styleUrls: const ['employment_component.css'])
+    styleUrls: const ['employment_component.css'],
+    directives: const [SubsectionComponent])
 class EmploymentComponent {
   @Input()
   String period;
